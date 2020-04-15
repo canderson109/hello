12bd619b2c24c0038e9c2059c25b8695b8c532ba
Conner Anderson <canderson109@mail.greenriver.edu>
2020-04-15 17:46:41 -0400

    added controller and homepage view

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+
+require_once('vendor/autoload.php');
+
+//Instantiate f3 base class
+
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function() {
+
+    // echo '<h1>Hello World!</h1>';
+    $view = new Template();
+    echo $view->render('views/home.html');
+
+
+});
+
+//Run fat free
+
+$f3->run();
